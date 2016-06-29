7ba10fbe53b8105b9f17e30faeb12040a0033abb
Rinat Khabibiev <srenskiy@gmail.com>
2016-06-29 17:46:44 +0300

    django project structure

---
+++ settings.py
@@ -0,0 +1,40 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'secret_key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'push',
+]
+
+MIDDLEWARE_CLASSES = []
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+TIME_ZONE = 'UTC'
+
+USE_TZ = True
