54234c14c5b215295a74b903bc75a43fa7297d50
Rinat Khabibiev <srenskiy@gmail.com>
2016-07-01 18:07:33 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.rst') as description:
+    long_description = description.read()
+
+setup(
+    name='django-push',
+    version='0.1',
+    author='Rinat Khabibiev',
+    author_email='srenskiy@gmail.com',
+    packages=list(map('push.'.__add__, find_packages('push'))) + ['push'],
+    url='https://github.com/renskiy/django-push',
+    license='MIT',
+    description='fabricio',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Communications',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django :: 1.9',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'Django>=1.9,<2.0',
+    ],
+)
